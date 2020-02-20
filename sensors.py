e81316984f5f26d0da7e7cd34174005dfcf46a12
Gus Mueller <gus@asecular.com>
2020-02-19 20:31:41 -0500

    fixed reading of data from the slave when the master sends data

--- sensors.py
+++ sensors.py
@@ -5,11 +5,11 @@ i2c_ch = 1
 bus = smbus.SMBus(i2c_ch)
 
 i2c_address = 20
-#still trying to figure out how to send a byte to a slave, see:
-#https://stackoverflow.com/questions/60292104/how-do-i-read-a-byte-from-a-slave-i2c-device-while-handling-a-request-from-the-m
-#bus.write_byte_data(i2c_address, 113,111)
-val = bus.read_i2c_block_data(i2c_address,12)
-#bus.write_byte(i2c_address, 123)
 
+bus.write_byte_data(i2c_address, 14, 17)
+val = bus.read_i2c_block_data(i2c_address,5)#get raw wind direction
+print(val)
+
+val = bus.read_i2c_block_data(i2c_address,3)#get  wind direction
 print(val)
 
